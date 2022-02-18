253c4350f9061ebb2546ed6b38bda7becf014f3a
mhubii <m.huber_1994@hotmail.de>
2022-02-18 19:19:15 +0000

    finished config, add qpoases and kdl

--- include/rvim_ros2_controllers_experimental/hand_guide_position_controller.hpp
+++ include/rvim_ros2_controllers_experimental/hand_guide_position_controller.hpp
@@ -3,14 +3,24 @@
 #include <vector>
 #include <string>
 
+#include <hardware_interface/loaned_state_interface.hpp>
+#include <hardware_interface/types/hardware_interface_type_values.hpp>
 #include <controller_interface/controller_interface.hpp>
+
+#include <qpOASES/QProblem.hpp>
+#include <kdl_parser/kdl_parser.hpp>
+#include <kdl/chain.hpp>
 
 // follow http://control.ros.org/ros2_controllers/doc/writing_new_controller.html
 // see forward command controller for an example: https://github.com/ros-controls/ros2_controllers/blob/foxy/forward_command_controller/include/forward_command_controller/forward_command_controller.hpp
+// cartesian controller: http://library.isr.ist.utl.pt/docs/roswiki/pr2_mechanism(2f)Tutorials(2f)Coding(20)a(20)realtime(20)Cartesian(20)controller(20)with(20)KDL.html
+// replicate previous hand guiding mode
 
 namespace rvim_ros2_controllers_experimental {
 
     using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+    constexpr char HW_IF_EXTERNAL_TORQUE[] = "external_torque";
 
     class HandGuidePositionController : public controller_interface::ControllerInterface {
         public:
@@ -28,9 +38,17 @@ namespace rvim_ros2_controllers_experimental {
 
         protected:
             std::vector<std::string> joint_names_;
-            std::vector<std::string> interface_names_;  // for parsing in yaml file (external_torque & position required)
+            std::string command_interface_name_;              // for parsing in yaml file (external_torque & position required)
+            std::vector<std::string> state_interface_names_;  // for parsing in yaml file (external_torque & position required)
+
+            std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>> position_interfaces_;
+            std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>> external_torque_interfaces_;
+
+            KDL::Tree tree_;
+            KDL::Chain kdl_chain_;
+
+            std::string chain_root_;
+            std::string chain_tip_;
     };
 
 }  // end of namespace rvim_ros2_controllers_experimental
-
-
--- src/hand_guide_position_controller.cpp
+++ src/hand_guide_position_controller.cpp
@@ -3,7 +3,7 @@
 namespace rvim_ros2_controllers_experimental {
 
     controller_interface::return_type HandGuidePositionController::init(const std::string& controller_name) {
-        auto ret = ControllerInterface::init(controller_name);  // required!
+        auto ret = ControllerInterface::init(controller_name);
         if (ret != controller_interface::return_type::OK) {
             return ret;
         }
@@ -12,33 +12,131 @@ namespace rvim_ros2_controllers_experimental {
     }
 
     controller_interface::InterfaceConfiguration HandGuidePositionController::command_interface_configuration() const {
-        // RCLCPP_INFO(this->get_node()->get_logger(), "Command interface configure called.");
-        return controller_interface::InterfaceConfiguration();
+        RCLCPP_INFO(node_->get_logger(), "Command interface configure called.");
+        controller_interface::InterfaceConfiguration command_interface_configuration;
+        command_interface_configuration.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+        for (const auto& joint_name: joint_names_) {
+            command_interface_configuration.names.push_back(joint_name + "/" + command_interface_name_);
+        }
+        return command_interface_configuration;
     }
 
     controller_interface::InterfaceConfiguration HandGuidePositionController::state_interface_configuration() const {
-        // RCLCPP_INFO(this->get_node()->get_logger(), "State interface configure called.");
-        return controller_interface::InterfaceConfiguration();
+        RCLCPP_INFO(node_->get_logger(), "State interface configure called.");
+        controller_interface::InterfaceConfiguration state_interface_configuration;
+        state_interface_configuration.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+        for (const auto& joint_name: joint_names_) {
+            for (const auto& state_interface: state_interface_names_) {
+                state_interface_configuration.names.push_back(joint_name + "/" + state_interface);
+            }
+        }
+        return state_interface_configuration;
     }
 
     CallbackReturn HandGuidePositionController::on_configure(const rclcpp_lifecycle::State& /*previous_state*/) {
-        // RCLCPP_INFO(this->get_node()->get_logger(), "On configure called.");
-        // print robot description?
+        // configure interfaces
+        if (!node_->get_parameter("command_interface", command_interface_name_)) {
+            RCLCPP_ERROR(node_->get_logger(), "Failed to load command_interface parameter");
+            return CallbackReturn::ERROR;
+        };
+        if (command_interface_name_ != hardware_interface::HW_IF_POSITION) {
+            RCLCPP_ERROR(node_->get_logger(), "Expected '%s' command interface, got '%s'.", hardware_interface::HW_IF_POSITION, command_interface_name_.c_str());
+            return CallbackReturn::ERROR;
+        }
+        if (!node_->get_parameter("state_interfaces", state_interface_names_)) {
+            RCLCPP_ERROR(node_->get_logger(), "Failed to load state_interfaces parameter.");
+            return CallbackReturn::ERROR;
+        };
+        if (!(
+            (std::find(state_interface_names_.begin(), state_interface_names_.end(), hardware_interface::HW_IF_POSITION) != state_interface_names_.end()) &&
+            (std::find(state_interface_names_.begin(), state_interface_names_.end(), HW_IF_EXTERNAL_TORQUE) != state_interface_names_.end())
+        )) {
+            RCLCPP_ERROR(node_->get_logger(), "Did not find expected '%s' and '%s' state interfaces.", hardware_interface::HW_IF_POSITION, HW_IF_EXTERNAL_TORQUE);
+            return CallbackReturn::ERROR;
+        }
+
+        // get joints
+        joint_names_ = node_->get_parameter("joints").as_string_array();
+        if (joint_names_.empty()) {
+            RCLCPP_ERROR(node_->get_logger(), "The joints parameter was empty.");
+            return CallbackReturn::ERROR;
+        }
+
+        // load robot description, see http://wiki.ros.org/kdl_parser/Tutorials/Start%20using%20the%20KDL%20parser
+        std::string robot_description_string = node_->get_parameter("robot_description").as_string();
+        if (!kdl_parser::treeFromString(robot_description_string, tree_)) {
+            RCLCPP_ERROR(node_->get_logger(), "Failed to construct kdl tree from robot description.");
+            return CallbackReturn::ERROR;
+        }
+
+        // configure kdl chain by defining root and tip from tree
+        if (!node_->get_parameter("chain_root", chain_root_)) {
+            RCLCPP_ERROR(node_->get_logger(), "Failed to load chain_root parameter, got '%s'.", chain_root_.c_str());
+            return CallbackReturn::ERROR;
+        };
+        if (!node_->get_parameter("chain_tip", chain_tip_)) {
+            RCLCPP_ERROR(node_->get_logger(), "Failed to load chain_tip parameter, got '%s'.", chain_tip_.c_str());
+            return CallbackReturn::ERROR;
+        };
+
+        if (!tree_.getChain(chain_root_, chain_tip_, kdl_chain_)) {
+            RCLCPP_ERROR(node_->get_logger(), "Failed to extract kdl chain with root '%s' and tip '%s'.", chain_root_.c_str(), chain_tip_.c_str());
+            return CallbackReturn::ERROR;
+        };
+
         return CallbackReturn::SUCCESS;
     }
 
     CallbackReturn HandGuidePositionController::on_activate(const rclcpp_lifecycle::State& /*previous_state*/) {
-        RCLCPP_INFO(this->get_node()->get_logger(), "On activate called.");
+        // get a sorted reference
+        for (auto& state_interface: state_interfaces_) {
+            if (state_interface.get_interface_name() == hardware_interface::HW_IF_POSITION) {
+                position_interfaces_.emplace_back(std::ref(state_interface));
+            } else if (state_interface.get_interface_name() == HW_IF_EXTERNAL_TORQUE) {
+                external_torque_interfaces_.emplace_back(std::ref(state_interface));
+            }
+            else {
+                RCLCPP_ERROR(node_->get_logger(), "Provided with wrong state interface '%s' for joint %s.", state_interface.get_interface_name().c_str(), state_interface.get_name().c_str());
+                return CallbackReturn::ERROR;
+            }
+        }
+
+        // check size of sorted references
+        if (joint_names_.size() != position_interfaces_.size()) {
+            RCLCPP_ERROR(node_->get_logger(), "Expected %zu %s interfaces, got %zu.", joint_names_.size(), hardware_interface::HW_IF_POSITION, position_interfaces_.size());
+            return CallbackReturn::ERROR;
+        }
+        if (joint_names_.size() != external_torque_interfaces_.size()) {
+            RCLCPP_ERROR(node_->get_logger(), "Expected %zu %s interfaces, got %zu", joint_names_.size(), HW_IF_EXTERNAL_TORQUE, external_torque_interfaces_.size());
+            return CallbackReturn::ERROR;
+        }
+
         return CallbackReturn::SUCCESS;
     }
 
     CallbackReturn HandGuidePositionController::on_deactivate(const rclcpp_lifecycle::State& /*previous_state*/) {
-        RCLCPP_INFO(this->get_node()->get_logger(), "On deactivate called.");
+        RCLCPP_INFO(node_->get_logger(), "On deactivate called.");
         return CallbackReturn::SUCCESS;
     }
 
     controller_interface::return_type HandGuidePositionController::update() {
-        RCLCPP_INFO(this->get_node()->get_logger(), "Update called.");
+
+        // noise filter!
+
+        // safety set 0!
+        for (std::size_t i = 0; i<command_interfaces_.size(); i++) {
+            // const auto& et = external_torque_interfaces_[i].get().get_value();
+            // if (std::abs(et) > 4.) {
+            //     RCLCPP_INFO(node_->get_logger(), "et [%zu]: %f", i, et);
+            //     auto sign = double(std::signbit(et));
+            //     command_interfaces_[i].set_value(position_interfaces_[i].get().get_value() - sign*0.005);
+            // } else {
+                command_interfaces_[i].set_value(position_interfaces_[i].get().get_value());
+            // }
+        }
+
+        // RCLCPP_INFO(node_->get_logger(), "position: %f, external torque: %f", position_interfaces_[0].get().get_value(), external_torque_interfaces_[0].get().get_value());
+
         return controller_interface::return_type::OK;
     }
 
