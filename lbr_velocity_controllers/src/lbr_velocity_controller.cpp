0b60bbaf2ddcad9947cc1a695629d688ed79539c
mhubii <m.huber_1994@hotmail.de>
2023-01-10 19:08:21 +0000

    fixed bug

--- lbr_velocity_controllers/src/lbr_velocity_controller.cpp
+++ lbr_velocity_controllers/src/lbr_velocity_controller.cpp
@@ -50,12 +50,6 @@ LBRVelocityController::on_configure(const rclcpp_lifecycle::State & /*previous_s
       get_node()->create_subscription<std_msgs::msg::Float64MultiArray>(
           "~/joint_velocity_command", rclcpp::SystemDefaultsQoS(),
           [this](const std_msgs::msg::Float64MultiArray::SharedPtr joint_velocity_command) {
-            if (joint_velocity_command->data.size() != lbr_fri_ros2::LBR::JOINT_DOF) {
-              RCLCPP_ERROR(get_node()->get_logger(),
-                           "Received velocity command of size %lu, expected %d.",
-                           joint_velocity_command->data.size(), lbr_fri_ros2::LBR::JOINT_DOF);
-              return;
-            }
             joint_velocity_command_rt_buffer_.writeFromNonRT(joint_velocity_command);
           });
   return controller_interface::CallbackReturn::SUCCESS;
@@ -95,20 +89,25 @@ LBRVelocityController::on_deactivate(const rclcpp_lifecycle::State & /*previous_
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
-controller_interface::return_type LBRVelocityController::update(const rclcpp::Time & /*time*/,
-                                                                const rclcpp::Duration &/*period*/) {
-  for (uint8_t i = 0; i < lbr_fri_ros2::LBR::JOINT_DOF; ++i) {
-    current_position_[i] = position_state_interfaces_[i].get().get_value();
-    current_velocity_[i] = velocity_state_interfaces_[i].get().get_value();
-  }
+controller_interface::return_type
+LBRVelocityController::update(const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/) {
   auto joint_velocity_command = joint_velocity_command_rt_buffer_.readFromRT();
   if (!joint_velocity_command || !(*joint_velocity_command)) {
     return controller_interface::return_type::OK;
   }
-  desired_velocity_ = (*joint_velocity_command)->data;
+  if ((*joint_velocity_command)->data.size() != desired_velocity_.size()) {
+    RCLCPP_ERROR(get_node()->get_logger(), "Expected velocity command of size %lu, got %lu.",
+                 desired_velocity_.size(), (*joint_velocity_command)->data.size());
+    return controller_interface::return_type::ERROR;
+  }
+  for (uint8_t i = 0; i < lbr_fri_ros2::LBR::JOINT_DOF; ++i) {
+    current_position_[i] = position_state_interfaces_[i].get().get_value();
+    current_velocity_[i] = velocity_state_interfaces_[i].get().get_value();
+    desired_velocity_[i] = (*joint_velocity_command)->data[i];
+  }
 
-  velocity_control_rule_->compute(current_position_, current_velocity_, desired_velocity_,
-                                  0.01, position_command_); // TODO: replace time!
+  velocity_control_rule_->compute(current_position_, current_velocity_, desired_velocity_, 0.01,
+                                  position_command_); // TODO: replace time!
   for (uint8_t i = 0; i < lbr_fri_ros2::LBR::JOINT_DOF; ++i) {
     position_command_interfaces_[i].get().set_value(position_command_[i]);
   }
