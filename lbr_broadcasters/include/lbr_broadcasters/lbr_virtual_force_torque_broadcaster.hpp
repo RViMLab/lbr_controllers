80c18fde06cfea15e595c5910b37f7606cdef4c4
mhubii <m.huber_1994@hotmail.de>
2023-03-13 22:56:38 +0000

    moved fiels

--- lbr_broadcasters/include/lbr_broadcasters/lbr_virtual_force_torque_broadcaster.hpp
+++ lbr_broadcasters/include/lbr_broadcasters/lbr_virtual_force_torque_broadcaster.hpp
@@ -1,12 +1,22 @@
 #ifndef LBR_BROADCASTERS__LBR_VIRTUAL_FORCE_TORQUE_BROADCASTER_HPP_
 #define LBR_BROADCASTERS__LBR_VIRTUAL_FORCE_TORQUE_BROADCASTER_HPP_
 
+#include <array>
+#include <limits>
 #include <memory>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
 #include "controller_interface/controller_interface.hpp"
-#include "geometry_msgs/msg/wrench.hpp"
+#include "geometry_msgs/msg/wrench_stamped.hpp"
+#include "hardware_interface/loaned_state_interface.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "realtime_tools/realtime_publisher.h"
+
+#include "lbr_fri_ros2/lbr.hpp"
+#include "lbr_hardware_interface/lbr_hardware_interface_type_values.hpp"
 
 namespace lbr_broadcasters {
 
@@ -31,9 +41,18 @@ public:
                                            const rclcpp::Duration &period) override;
 
 protected:
-  rclcpp::Publisher<geometry_msgs::msg::Wrench>::SharedPtr virtual_force_torque_publisher_;
-  std::unique_ptr<realtime_tools::RealtimePublisher<geometry_msgs::msg::Wrench>>
+  bool reference_state_interfaces_();
+  bool clear_state_interfaces_();
+
+  rclcpp::Publisher<geometry_msgs::msg::WrenchStamped>::SharedPtr virtual_force_torque_publisher_;
+  std::unique_ptr<realtime_tools::RealtimePublisher<geometry_msgs::msg::WrenchStamped>>
       virtual_force_torque_realtime_publisher_;
+  std::array<std::string, 2> state_interface_names_;
+  std::vector<std::string> joint_names_;
+  std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>>
+      position_state_interfaces_;
+  std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>>
+      external_torque_state_interfaces_;
 };
 
 } // end of namespace lbr_broadcasters
---
+++ lbr_broadcasters/src/lbr_virtual_force_torque_broadcaster.cpp
@@ -0,0 +1,98 @@
+#include "lbr_broadcasters/lbr_virtual_force_torque_broadcaster.hpp"
+
+namespace lbr_broadcasters {
+
+LBRVirtualForceTorqueBroadcaster::LBRVirtualForceTorqueBroadcaster()
+    : virtual_force_torque_publisher_(nullptr), virtual_force_torque_realtime_publisher_(nullptr),
+      state_interface_names_{lbr_hardware_interface::HW_IF_EXTERNAL_TORQUE,
+                             hardware_interface::HW_IF_POSITION} {}
+
+controller_interface::InterfaceConfiguration
+LBRVirtualForceTorqueBroadcaster::command_interface_configuration() const {
+  return controller_interface::InterfaceConfiguration{
+      controller_interface::interface_configuration_type::NONE};
+}
+
+controller_interface::InterfaceConfiguration
+LBRVirtualForceTorqueBroadcaster::state_interface_configuration() const {
+  controller_interface::InterfaceConfiguration interface_configuration;
+  interface_configuration.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+  for (const auto &joint_name : joint_names_) {
+    for (const auto &state_interface_name : state_interface_names_) {
+      interface_configuration.names.push_back(joint_name + "/" + state_interface_name);
+    }
+  }
+  return interface_configuration;
+}
+
+controller_interface::CallbackReturn LBRVirtualForceTorqueBroadcaster::on_init() {}
+
+controller_interface::CallbackReturn
+LBRVirtualForceTorqueBroadcaster::on_configure(const rclcpp_lifecycle::State &previous_state) {}
+
+controller_interface::CallbackReturn
+LBRVirtualForceTorqueBroadcaster::on_activate(const rclcpp_lifecycle::State &previous_state) {
+  if (!clear_state_interfaces_()) {
+    return controller_interface::CallbackReturn::ERROR;
+  }
+  if (!reference_state_interfaces_()) {
+    return controller_interface::CallbackReturn::ERROR;
+  }
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn
+LBRVirtualForceTorqueBroadcaster::on_deactivate(const rclcpp_lifecycle::State &previous_state) {
+  if (!clear_state_interfaces_()) {
+    return controller_interface::CallbackReturn::ERROR;
+  }
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::return_type
+LBRVirtualForceTorqueBroadcaster::update(const rclcpp::Time &time, const rclcpp::Duration &period) {
+  // compute j pseudo inverse times ext
+  RCLCPP_INFO(get_node()->get_logger(), "update");
+}
+
+bool LBRVirtualForceTorqueBroadcaster::reference_state_interfaces_() {
+  try {
+    for (auto &state_interface : state_interfaces_) {
+      if (state_interface.get_interface_name() == hardware_interface::HW_IF_POSITION) {
+        position_state_interfaces_.emplace_back(std::ref(state_interface));
+      }
+      if (state_interface.get_interface_name() == lbr_hardware_interface::HW_IF_EXTERNAL_TORQUE) {
+        external_torque_state_interfaces_.emplace_back(std::ref(state_interface));
+      }
+    }
+    if (position_state_interfaces_.size() != lbr_fri_ros2::LBR::JOINT_DOF) {
+      RCLCPP_ERROR(get_node()->get_logger(), "Expected %d position state interfaces, got %lu.",
+                   lbr_fri_ros2::LBR::JOINT_DOF, position_state_interfaces_.size());
+      return false;
+    }
+    if (external_torque_state_interfaces_.size() != lbr_fri_ros2::LBR::JOINT_DOF) {
+      RCLCPP_ERROR(get_node()->get_logger(),
+                   "Expected %d external torque state interfaces, got %lu.",
+                   lbr_fri_ros2::LBR::JOINT_DOF, external_torque_state_interfaces_.size());
+      return false;
+    }
+  } catch (const std::exception &e) {
+    RCLCPP_ERROR(get_node()->get_logger(),
+                 "Exception thrown while referencing state interfaces.\n%s", e.what());
+    return false;
+  }
+  return true;
+}
+
+bool LBRVirtualForceTorqueBroadcaster::clear_state_interfaces_() {
+  position_state_interfaces_.clear();
+  external_torque_state_interfaces_.clear();
+  return true;
+}
+
+} // end of namespace lbr_broadcasters
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(lbr_broadcasters::LBRVirtualForceTorqueBroadcaster,
+                       controller_interface::ControllerInterface)
