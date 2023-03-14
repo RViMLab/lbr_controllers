420a0dc56b10bd58e1f6fd9c4377c9edb6dc9785
mhubii <m.huber_1994@hotmail.de>
2023-03-14 12:21:53 +0000

    finished virtual sensor

--- lbr_broadcasters/src/lbr_virtual_force_torque_broadcaster.cpp
+++ lbr_broadcasters/src/lbr_virtual_force_torque_broadcaster.cpp
@@ -3,9 +3,17 @@
 namespace lbr_broadcasters {
 
 LBRVirtualForceTorqueBroadcaster::LBRVirtualForceTorqueBroadcaster()
-    : virtual_force_torque_publisher_(nullptr), virtual_force_torque_realtime_publisher_(nullptr),
+    : force_torque_publisher_(nullptr), force_torque_realtime_publisher_(nullptr),
       state_interface_names_{lbr_hardware_interface::HW_IF_EXTERNAL_TORQUE,
-                             hardware_interface::HW_IF_POSITION} {}
+                             hardware_interface::HW_IF_POSITION},
+      sensitivity_offset_(lbr_fri_ros2::LBR::CARTESIAN_DOF,
+                          std::numeric_limits<double>::infinity()),
+      singular_damping_(std::numeric_limits<double>::infinity()),
+      jacobian_(lbr_fri_ros2::LBR::CARTESIAN_DOF, lbr_fri_ros2::LBR::JOINT_DOF) {
+  positions_.setConstant(std::numeric_limits<double>::quiet_NaN());
+  external_torques_.setConstant(std::numeric_limits<double>::quiet_NaN());
+  force_torque_.setConstant(std::numeric_limits<double>::quiet_NaN());
+}
 
 controller_interface::InterfaceConfiguration
 LBRVirtualForceTorqueBroadcaster::command_interface_configuration() const {
@@ -25,18 +33,29 @@ LBRVirtualForceTorqueBroadcaster::state_interface_configuration() const {
   return interface_configuration;
 }
 
-controller_interface::CallbackReturn LBRVirtualForceTorqueBroadcaster::on_init() {}
+controller_interface::CallbackReturn LBRVirtualForceTorqueBroadcaster::on_init() {
+  force_torque_publisher_ = get_node()->create_publisher<geometry_msgs::msg::WrenchStamped>(
+      "~/force_torque", rclcpp::SystemDefaultsQoS());
+  force_torque_realtime_publisher_ =
+      std::make_unique<realtime_tools::RealtimePublisher<geometry_msgs::msg::WrenchStamped>>(
+          force_torque_publisher_);
+
+  return controller_interface::CallbackReturn::SUCCESS;
+}
 
 controller_interface::CallbackReturn
-LBRVirtualForceTorqueBroadcaster::on_configure(const rclcpp_lifecycle::State &previous_state) {
+LBRVirtualForceTorqueBroadcaster::on_configure(const rclcpp_lifecycle::State & /*previous_state*/) {
   if (!read_parameters_()) {
+    return controller_interface::CallbackReturn::ERROR;
+  }
+  if (!initialize_kinematics_()) {
     return controller_interface::CallbackReturn::ERROR;
   }
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
 controller_interface::CallbackReturn
-LBRVirtualForceTorqueBroadcaster::on_activate(const rclcpp_lifecycle::State &previous_state) {
+LBRVirtualForceTorqueBroadcaster::on_activate(const rclcpp_lifecycle::State & /*previous_state*/) {
   if (!clear_state_interfaces_()) {
     return controller_interface::CallbackReturn::ERROR;
   }
@@ -46,8 +65,8 @@ LBRVirtualForceTorqueBroadcaster::on_activate(const rclcpp_lifecycle::State &pre
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
-controller_interface::CallbackReturn
-LBRVirtualForceTorqueBroadcaster::on_deactivate(const rclcpp_lifecycle::State &previous_state) {
+controller_interface::CallbackReturn LBRVirtualForceTorqueBroadcaster::on_deactivate(
+    const rclcpp_lifecycle::State & /*previous_state*/) {
   if (!clear_state_interfaces_()) {
     return controller_interface::CallbackReturn::ERROR;
   }
@@ -55,16 +74,41 @@ LBRVirtualForceTorqueBroadcaster::on_deactivate(const rclcpp_lifecycle::State &p
 }
 
 controller_interface::return_type
-LBRVirtualForceTorqueBroadcaster::update(const rclcpp::Time &time, const rclcpp::Duration &period) {
-  // compute j pseudo inverse times ext
-  RCLCPP_INFO(get_node()->get_logger(), "external torques: %f, %f, %f, %f, %f, %f, %f",
-              external_torque_state_interfaces_[0].get().get_value(),
-              external_torque_state_interfaces_[1].get().get_value(),
-              external_torque_state_interfaces_[2].get().get_value(),
-              external_torque_state_interfaces_[3].get().get_value(),
-              external_torque_state_interfaces_[4].get().get_value(),
-              external_torque_state_interfaces_[5].get().get_value(),
-              external_torque_state_interfaces_[6].get().get_value());
+LBRVirtualForceTorqueBroadcaster::update(const rclcpp::Time & /*time*/,
+                                         const rclcpp::Duration & /*period*/) {
+  for (std::size_t i = 0; i < lbr_fri_ros2::LBR::JOINT_DOF; ++i) {
+    positions_[i] = position_state_interfaces_[i].get().get_value();
+    external_torques_[i] = external_torque_state_interfaces_[i].get().get_value();
+  }
+
+  // calculate the Jacobian
+  kinematics_->calculate_jacobian(positions_, end_effector_name_, jacobian_);
+
+  // calculate the force torque
+  force_torque_ =
+      damped_leaste_squares(jacobian_, singular_damping_).transpose() * external_torques_;
+
+  force_torque_ = force_torque_.NullaryExpr([this](const Eigen::Index &i) {
+    double sign = std::signbit(force_torque_[i]) ? -1.0 : 1.0;
+    return std::abs(force_torque_[i]) < sensitivity_offset_[i]
+               ? 0.0
+               : sign * (std::abs(force_torque_[i]) - sensitivity_offset_[i]);
+  });
+
+  // publish force torque
+  if (force_torque_realtime_publisher_->trylock()) {
+    force_torque_msg_.header.frame_id = end_effector_name_;
+    // force_torque_msg_.header.stamp.
+    force_torque_msg_.wrench.force.x = force_torque_[0];
+    force_torque_msg_.wrench.force.y = force_torque_[1];
+    force_torque_msg_.wrench.force.z = force_torque_[2];
+    force_torque_msg_.wrench.torque.x = force_torque_[3];
+    force_torque_msg_.wrench.torque.y = force_torque_[4];
+    force_torque_msg_.wrench.torque.z = force_torque_[5];
+    force_torque_realtime_publisher_->msg_ = force_torque_msg_;
+    force_torque_realtime_publisher_->unlockAndPublish();
+  }
+
   return controller_interface::return_type::OK;
 }
 
@@ -81,6 +125,27 @@ bool LBRVirtualForceTorqueBroadcaster::read_parameters_() {
     if (joint_names_.size() != lbr_fri_ros2::LBR::JOINT_DOF) {
       RCLCPP_ERROR(get_node()->get_logger(), "Expected %lu joint names, got %d.",
                    joint_names_.size(), lbr_fri_ros2::LBR::JOINT_DOF);
+      return false;
+    }
+    if (!get_node()->get_parameter("kinematics_plugin", kinematics_plugin_name_)) {
+      RCLCPP_ERROR(get_node()->get_logger(), "Failed to retrieve kinematics plugin parameter.");
+      return false;
+    }
+    if (!get_node()->get_parameter("end_effector_name", end_effector_name_)) {
+      RCLCPP_ERROR(get_node()->get_logger(), "Failed to retrieve end effector name parameter.");
+      return false;
+    }
+    if (!get_node()->get_parameter("sensitivity_offset", sensitivity_offset_)) {
+      RCLCPP_ERROR(get_node()->get_logger(), "Failed to retrieve sensitivity offset parameter.");
+      return false;
+    }
+    if (sensitivity_offset_.size() != lbr_fri_ros2::LBR::CARTESIAN_DOF) {
+      RCLCPP_ERROR(get_node()->get_logger(), "Expected %lu sensitivity offset values, got %d.",
+                   sensitivity_offset_.size(), lbr_fri_ros2::LBR::CARTESIAN_DOF);
+      return false;
+    }
+    if (!get_node()->get_parameter("singular_damping", singular_damping_)) {
+      RCLCPP_ERROR(get_node()->get_logger(), "Failed to retrieve singular damping parameter.");
       return false;
     }
   } catch (const std::exception &e) {
@@ -122,6 +187,24 @@ bool LBRVirtualForceTorqueBroadcaster::reference_state_interfaces_() {
 bool LBRVirtualForceTorqueBroadcaster::clear_state_interfaces_() {
   position_state_interfaces_.clear();
   external_torque_state_interfaces_.clear();
+  positions_.setConstant(std::numeric_limits<double>::quiet_NaN());
+  external_torques_.setConstant(std::numeric_limits<double>::quiet_NaN());
+  force_torque_.setConstant(std::numeric_limits<double>::quiet_NaN());
+  return true;
+}
+
+bool LBRVirtualForceTorqueBroadcaster::initialize_kinematics_() {
+  try {
+    kinematics_loader_ =
+        std::make_shared<pluginlib::ClassLoader<kinematics_interface::KinematicsInterface>>(
+            "kinematics_interface", "kinematics_interface::KinematicsInterface");
+    kinematics_ = std::unique_ptr<kinematics_interface::KinematicsInterface>(
+        kinematics_loader_->createUnmanagedInstance(kinematics_plugin_name_));
+    kinematics_->initialize(get_node()->get_node_parameters_interface(), end_effector_name_);
+  } catch (const std::exception &e) {
+    RCLCPP_ERROR(get_node()->get_logger(), "Failed to initialize kinematics.\n%s", e.what());
+    return false;
+  }
   return true;
 }
 
